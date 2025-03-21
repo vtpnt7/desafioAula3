bc4d7133ab6ff73aba78363095e85dad5ad02e29
Victor Hugo Campedelli <vhvc@live.com>
2025-03-21 19:52:30 -0300

    Primeiro commit

---
+++ app.js
@@ -0,0 +1,22 @@
+//1. Lista vazia
+let listaGenerica = [];
+
+//2. Lista de linguagens de programação chamada linguagensDeProgramacao com os seguintes elementos: 'JavaScript','C','C++', 'Kotlin' e 'Python'.
+let linguagensDeProgramacao = ['JavaScript', 'C', 'C++', 'Kotlin', 'Python'];
+console.log (linguagensDeProgramacao);
+
+//3. Adicione à lista linguagensDeProgramacao os seguintes elementos: 'Java', 'Ruby' e 'GoLang'.
+linguagensDeProgramacao.push('Java');
+linguagensDeProgramacao.push('Ruby');
+linguagensDeProgramacao.push('GoLang');
+console.log(linguagensDeProgramacao);
+
+//4.Crie uma lista com 3 nomes e exiba no console apenas o primeiro elemento.
+let listaNomes = ['Joao', 'Maria', 'Jose'];
+console.log(listaNomes[0]);
+
+//5. Crie uma lista com 3 nomes e exiba no console apenas o segundo elemento. 
+console.log(listaNomes[1]);
+
+//6. Crie uma lista com 3 nomes e exiba no console apenas o último elemento.
+console.log(listaNomes[2]);
